75a442353821cf7763687745ca57836e11ba2e54
gaurav <gaurav.joshi082004@gmail.com>
2026-03-04 20:22:25 +0530

    tested api key

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { fetchphotos, fetchvideos } from './api/mediaapi'
 
 const App = () => {
+  function getphotos() {
+    fetchphotos()
+  }
   return (
     <div className='h-screen text-white w-full bg-gray-600'>
-      <h1 className='text-6xl'>hello world</h1>
+      <button onClick={async () => {
+        const data =await fetchphotos('cat')
+        console.log(data.results);
+        
+        
+      }}>Get Photos</button>
+
+      <button onClick={async () => {
+        const data =await fetchvideos('cat')
+        console.log(data.videos);
+        
+        
+      }}>Get Videos</button>
     </div>
   )
 }
